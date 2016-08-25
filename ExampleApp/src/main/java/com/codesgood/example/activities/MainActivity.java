7e537cbf4cf5ba61e577afa15d9b3f75188bd33b
Amilcar Serrano <aserrano@phunware.com>
2016-08-25 11:16:16 -0600

    Minor changes in example app

--- ExampleApp/src/main/java/com/codesgood/example/activities/MainActivity.java
+++ ExampleApp/src/main/java/com/codesgood/example/activities/MainActivity.java
@@ -1,44 +1,16 @@
 package com.codesgood.example.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.text.Html;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 import com.codesgood.justifiedtext.R;
-import com.codesgood.views.JustifiedTextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView view = (TextView) findViewById(R.id.text_one);
-        view.setText(R.string.lorem_ipsum_extended);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
