e289a6629ff655990e5dc7a5dd9a44d02a0abfd2
Jimmy Marquez <jimmy.marquez@domandtom.com>
2016-12-31 14:55:09 -0600

    Adding support to Html.fromHtml, replacing String vars for stringBuilders to avoid performance Issues.

--- ExampleApp/src/main/java/com/codesgood/example/activities/MainActivity.java
+++ ExampleApp/src/main/java/com/codesgood/example/activities/MainActivity.java
@@ -1,8 +1,15 @@
 package com.codesgood.example.activities;
 
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import android.text.Html;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.widget.TextView;
+import com.codesgood.example.activities.util.StringHtmlUtils;
 import com.codesgood.justifiedtext.R;
 
 
@@ -12,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String strTest = getResources().getString(R.string.text_sample_styled);
+        TextView txt = (TextView)findViewById(R.id.text_two);
+        txt.setText(StringHtmlUtils.fromHtml(strTest));
     }
 }
